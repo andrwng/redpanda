81c50ec80a8d6e5be66d567c45ae83072043ad15
NyaliaLui <nyalia@redpanda.com>
2022-05-26 21:03:44 -0400

    tests: use auto respond to apt

--- tests/rptest/scale_tests/upgrade_availability_test.py
+++ tests/rptest/scale_tests/upgrade_availability_test.py
@@ -2,6 +2,7 @@ from rptest.services.cluster import cluster
 from rptest.clients.types import TopicSpec
 from ducktape.utils.util import wait_until
 from rptest.tests.redpanda_test import RedpandaTest
+from rptest.services.redpanda import CHAOS_LOG_ALLOW_LIST
 from rptest.services.kafka_cli_consumer import KafkaCliConsumer
 from rptest.services.kafka_cli_producer import KafkaCliProducer
 import time
@@ -31,11 +32,18 @@ class UpgradeAvailabilityTest(RedpandaTest):
                 cmd = "curl -1sLf 'https://packages.vectorized.io/nzc4ZYQK3WRGd9sy/redpanda/cfg/setup/bash.deb.sh' | sudo -E bash"
                 node.account.ssh(cmd, allow_fail=False)
 
+        # Use 21.11.15 at startup
+        cmd = 'sudo apt -o  Dpkg::Options::="--force-confnew" install -y --allow-downgrades redpanda=21.11.15-1-7325762b'
+        for node in self.redpanda.nodes:
+            for line in node.account.ssh_capture(cmd, allow_fail=False):
+                line = line.strip()
+                self.logger.info(f'{node.name} apt result: {line}')
+
         super().setUp()  # RP nodes start in super
 
     def restart_node_w_version(self, node, version: str):
         self.redpanda.stop_node(node, timeout=300)
-        cmd = f'sudo apt -o  Dpkg::Options::="--force-confnew" install redpanda={version}'
+        cmd = f'sudo apt -o  Dpkg::Options::="--force-confnew" install -y --allow-downgrades redpanda={version}'
         node.account.ssh(cmd, allow_fail=False)
         # node.account.ssh("sudo systemctl stop redpanda")
         self.redpanda.start_node(node, None, timeout=300)
@@ -44,7 +52,6 @@ class UpgradeAvailabilityTest(RedpandaTest):
         for node in self.redpanda.nodes:
             self.restart_node_w_version(node, version)
 
-    # TODO: See how many producers and consumers to use.
     # Each producer writes 600Mbps and the consumers read until completion.
     def start_workload(self, runtime: int):
         if self.producer is not None:
@@ -66,20 +73,20 @@ class UpgradeAvailabilityTest(RedpandaTest):
                                          self.redpanda,
                                          self.topic,
                                          from_beginning=True,
-                                         kafka_version='2.8.0')
+                                         kafka_version='2.8.0',
+                                         save_msgs=False)
         self.consumer.start()
+        time.sleep(1)
         # Make sure consumer is recieving some data
-        self.consumer.wait_for_messages(10)
+        # self.consumer.wait_for_messages(10)
 
+    def wait_on_workload(self):
         # Terminate when the producer finishes
         self.producer.wait()
         self.consumer.stop()
 
-    @cluster(num_nodes=10)
+    @cluster(num_nodes=10, log_allow_list=CHAOS_LOG_ALLOW_LIST)
     def upgrade_availability_test(self):
-
-        # Use v21.11.15-1-7325762b on all nodes
-        self.restart_nodes_w_version('21.11.15-1-7325762b')
 
         # Start workload
         self.start_workload(runtime=30)
@@ -87,3 +94,5 @@ class UpgradeAvailabilityTest(RedpandaTest):
         # Upgrade one of the nodes to 22.1.3
         node = choice(self.redpanda.nodes)
         self.restart_node_w_version(node, '22.1.3-1')
+
+        self.wait_on_workload()
