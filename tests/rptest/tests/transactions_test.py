9243c8d74e797294b8fc0c1b1c47b778dbcfdb0d
Andrew Wong <awong@redpanda.com>
2022-07-15 12:36:40 -0700

    tests: add mixed-version test for transactions
    This adds a test that exercises RPC traffic for the transactions
    subsystem during intermediate stages of a rolling upgrade and rollback.
    The primary goal is to exercise RPCs across differently-versioned nodes.

--- tests/rptest/tests/transactions_test.py
+++ tests/rptest/tests/transactions_test.py
@@ -7,36 +7,33 @@
 # the Business Source License, use of this software will be governed
 # by the Apache License, Version 2.0
 
-from rptest.services.cluster import cluster
-from rptest.util import wait_until_result
+from ducktape.utils.util import wait_until
 from rptest.clients.types import TopicSpec
-
-import time
-
+from rptest.services.cluster import cluster
 from rptest.tests.redpanda_test import RedpandaTest
-from rptest.services.rpk_consumer import RpkConsumer
+from rptest.tests.upgrade_with_workload import MixedVersionWorkloadRunner
+from rptest.util import wait_until_result
+
 import confluent_kafka as ck
-from rptest.clients.kafka_cat import KafkaCat
-
-import subprocess
 
 
-class TransactionsTest(RedpandaTest):
+class TransactionsTestBase(RedpandaTest):
     topics = (TopicSpec(partition_count=1, replication_factor=3),
               TopicSpec(partition_count=1, replication_factor=3))
 
-    def __init__(self, test_context):
-        extra_rp_conf = {
-            "enable_idempotence": True,
-            "enable_transactions": True,
-            "transaction_coordinator_replication": 3,
-            "id_allocator_replication": 3,
-            "enable_leader_balancer": False,
-            "partition_autobalancing_mode": "off",
-        }
+    def __init__(self, test_context, extra_rp_conf=None):
+        if not extra_rp_conf:
+            extra_rp_conf = {
+                "enable_idempotence": True,
+                "enable_transactions": True,
+                "transaction_coordinator_replication": 3,
+                "id_allocator_replication": 3,
+                "enable_leader_balancer": False,
+                "partition_autobalancing_mode": "off",
+            }
 
-        super(TransactionsTest, self).__init__(test_context=test_context,
-                                               extra_rp_conf=extra_rp_conf)
+        super(TransactionsTestBase, self).__init__(test_context=test_context,
+                                                   extra_rp_conf=extra_rp_conf)
 
         self.input_t = self.topics[0]
         self.output_t = self.topics[1]
@@ -71,6 +68,14 @@ class TransactionsTest(RedpandaTest):
                                  timeout_sec=30,
                                  backoff_sec=2,
                                  err_msg="Can not consume data")
+
+
+class TransactionsTest(TransactionsTestBase):
+    topics = (TopicSpec(partition_count=1, replication_factor=3),
+              TopicSpec(partition_count=1, replication_factor=3))
+
+    def __init__(self, test_context):
+        super(TransactionsTest, self).__init__(test_context)
 
     @cluster(num_nodes=3)
     def simple_test(self):
@@ -244,3 +249,110 @@ class TransactionsTest(RedpandaTest):
             assert kafka_error.code() == ck.cimpl.KafkaError.FENCED_INSTANCE_ID
 
         producer.abort_transaction()
+
+
+class MixedVersionTransactionsTest(TransactionsTestBase):
+    def __init__(self, test_context):
+        extra_rp_conf = {
+            "default_topic_replications": 3,
+            "enable_auto_rebalance_on_node_add": False,
+            "enable_idempotence": True,
+            "enable_leader_balancer": False,
+            "enable_transactions": True,
+            "id_allocator_replication": 3,
+            "group_topic_partitions": 1,
+            "transaction_coordinator_replication": 3,
+        }
+        super(MixedVersionTransactionsTest,
+              self).__init__(test_context=test_context,
+                             extra_rp_conf=extra_rp_conf)
+        self.initial_version = MixedVersionWorkloadRunner.PRE_SERDE_VERSION
+        self.txn_id = 0
+
+    def setUp(self):
+        self.redpanda._installer.install(self.redpanda.nodes,
+                                         self.initial_version)
+        super(MixedVersionTransactionsTest, self).setUp()
+
+    def txn_workload(self, src_node, dst_node):
+        src_id = self.redpanda.idx(src_node)
+        dst_id = self.redpanda.idx(dst_node)
+        admin = self.redpanda._admin
+        # Have the transaction coordinator start out on 'src_node'.
+        admin.transfer_leadership_to(namespace="kafka_internal",
+                                     topic="tx",
+                                     partition=0,
+                                     target_id=src_id)
+        # The receivers of transaction coordinator RPCs (the leader of the
+        # topic being written to and the group topic) should be on 'dst_node'.
+        admin.transfer_leadership_to(namespace="kafka",
+                                     topic="__consumer_offsets",
+                                     partition=0,
+                                     target_id=dst_id)
+        admin.transfer_leadership_to(namespace="kafka",
+                                     topic=self.output_t.name,
+                                     partition=0,
+                                     target_id=dst_id)
+
+        def run_txn(should_commit):
+            self.generate_data(self.output_t, self.max_records)
+            producer = ck.Producer({
+                'bootstrap.servers': self.redpanda.brokers(),
+                'transactional.id': str(self.txn_id),
+            })
+            producer.init_transactions()
+            consumer = ck.Consumer({
+                'bootstrap.servers': self.redpanda.brokers(),
+                'group.id': "test",
+                'auto.offset.reset': 'earliest',
+                'enable.auto.commit': False,
+            })
+
+            consumer.subscribe([self.output_t])
+            num_consumed_records = 0
+
+            producer.begin_transaction()
+            while num_consumed_records < self.max_records:
+                records = self.consume(consumer)
+                for r in records:
+                    producer.produce(self.output_t.name, r.value(), r.key())
+                num_consumed_records += len(records)
+
+            producer.send_offsets_to_transaction(
+                consumer.position(consumer.assignment()),
+                consumer.consumer_group_metadata())
+
+            if should_commit:
+                producer.commit_transaction()
+            else:
+                producer.abort_transaction()
+            producer.flush()
+            consumer.close()
+
+            self.txn_id += 1
+
+        run_txn(should_commit=True)
+        run_txn(should_commit=False)
+
+    @cluster(num_nodes=3)
+    def test_txn_rpcs_with_upgrade(self):
+        # Initialize a transaction to bootstrap our transaction internal topic.
+        producer = ck.Producer({
+            'bootstrap.servers': self.redpanda.brokers(),
+            'transactional.id': str(self.txn_id),
+        })
+        producer.init_transactions()
+        self.generate_data(self.output_t, self.max_records)
+        self.txn_id += 1
+        consumer = ck.Consumer({
+            'bootstrap.servers': self.redpanda.brokers(),
+            'group.id': "dummy",
+            'auto.offset.reset': 'earliest',
+            'enable.auto.commit': False,
+        })
+        consumer.subscribe([self.output_t])
+        self.consume(consumer)
+
+        MixedVersionWorkloadRunner.upgrade_with_workload(
+            self.redpanda, MixedVersionWorkloadRunner.PRE_SERDE_VERSION,
+            self.txn_workload)
