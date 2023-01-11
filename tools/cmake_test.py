8f27f528772b1103d86084a34ce888d20c48992d
Andrew Wong <awong@redpanda.com>
2023-01-11 12:51:43 -0800

    info logging

--- tools/cmake_test.py
+++ tools/cmake_test.py
@@ -195,7 +195,7 @@ class TestRunner():
         if "rpunit" in binary or "rpfixture" in binary:
             unit_args = [
                 "--overprovisioned", "--unsafe-bypass-fsync 1",
-                "--default-log-level=trace", "--logger-log-level='io=debug'",
+                "--default-log-level=info", "--logger-log-level='io=debug'",
                 "--logger-log-level='exception=debug'"
             ] + COMMON_TEST_ARGS
             if "--" in args:
