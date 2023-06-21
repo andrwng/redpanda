dcfbbb0c4650bc24f862f92d95c45f2de635f5d3
Andrew Wong <awong@redpanda.com>
2023-06-21 01:21:08 -0700

    cloud_storage: test for delete records on spillover region

--- src/v/cloud_storage/tests/delete_records_e2e_test.cc
+++ src/v/cloud_storage/tests/delete_records_e2e_test.cc
@@ -20,6 +20,7 @@
 
 #include <seastar/core/io_priority_class.hh>
 
+#include <absl/container/flat_hash_set.h>
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/test/tools/old/interface.hpp>
 
@@ -32,11 +33,30 @@ using tests::kv_t;
 
 static ss::logger e2e_test_log("delete_records_e2e_test");
 
+namespace {
+
+void check_consume_out_of_range(
+  kafka_consume_transport& consumer,
+  const model::topic& topic_name,
+  const model::partition_id pid,
+  model::offset kafka_offset) {
+    BOOST_REQUIRE_EXCEPTION(
+      consumer.consume_from_partition(topic_name, pid, kafka_offset).get(),
+      std::runtime_error,
+      [](std::runtime_error e) {
+          return std::string(e.what()).find("out_of_range")
+                 != std::string::npos;
+      });
+};
+
+} // namespace
+
 class delete_records_e2e_fixture
   : public s3_imposter_fixture
   , public redpanda_thread_fixture
   , public enable_cloud_storage_fixture {
 public:
+    static constexpr auto segs_per_spill = 10;
     delete_records_e2e_fixture()
       : redpanda_thread_fixture(
         redpanda_thread_fixture::init_cloud_storage_tag{},
@@ -44,23 +64,38 @@ public:
         // No expectations: tests will PUT and GET organically.
         set_expectations_and_listen({});
         wait_for_controller_leadership().get();
+
+        // Apply local retention frequently.
         config::shard_local_cfg().log_compaction_interval_ms.set_value(
           std::chrono::duration_cast<std::chrono::milliseconds>(1s));
+        // Avoid segment merging since we'll controller uploads ourselves.
+        config::shard_local_cfg()
+          .cloud_storage_enable_segment_merging.set_value(false);
+        // Disable metrics to speed things up.
+        config::shard_local_cfg().enable_metrics_reporter.set_value(false);
+        // Encourage spilling over.
+        config::shard_local_cfg()
+          .cloud_storage_spillover_manifest_max_segments.set_value(
+            std::make_optional<size_t>(segs_per_spill));
+
+        topic_name = model::topic("tapioca");
+        ntp = model::ntp(model::kafka_namespace, topic_name, 0);
+
+        // Create a tiered storage topic with very little local retention.
+        cluster::topic_properties props;
+        props.shadow_indexing = model::shadow_indexing_mode::full;
+        props.retention_local_target_bytes = tristate<size_t>(1);
+        props.cleanup_policy_bitflags
+          = model::cleanup_policy_bitflags::deletion;
+        add_topic({model::kafka_namespace, topic_name}, 1, props).get();
+        wait_for_leader(ntp).get();
     }
+
+    model::topic topic_name;
+    model::ntp ntp;
 };
 
-FIXTURE_TEST(test_delete_local, delete_records_e2e_fixture) {
-    const model::topic topic_name("tapioca");
-    model::ntp ntp(model::kafka_namespace, topic_name, 0);
-    cluster::topic_properties props;
-    props.shadow_indexing = model::shadow_indexing_mode::full;
-    props.retention_local_target_bytes = tristate<size_t>(1);
-    props.cleanup_policy_bitflags = model::cleanup_policy_bitflags::deletion;
-    add_topic({model::kafka_namespace, topic_name}, 1, props).get();
-    wait_for_leader(ntp).get();
-
-    // Do some sanity checks that our partition looks the way we expect (has a
-    // log, archiver, etc).
+FIXTURE_TEST(test_delete_from_stm, delete_records_e2e_fixture) {
     auto partition = app.partition_manager.local().get(ntp);
     auto* log = dynamic_cast<storage::disk_log_impl*>(
       partition->log().get_impl());
@@ -107,18 +142,6 @@ FIXTURE_TEST(test_delete_local, delete_records_e2e_fixture) {
 
     kafka_consume_transport consumer(make_kafka_client().get());
     consumer.start().get();
-    const auto check_consume_out_of_range = [&](model::offset kafka_offset) {
-        BOOST_REQUIRE_EXCEPTION(
-          consumer
-            .consume_from_partition(
-              topic_name, model::partition_id(0), kafka_offset)
-            .get(),
-          std::runtime_error,
-          [](std::runtime_error e) {
-              return std::string(e.what()).find("out_of_range")
-                     != std::string::npos;
-          });
-    };
     auto consumed_records = consumer
                               .consume_from_partition(
                                 topic_name,
@@ -130,5 +153,74 @@ FIXTURE_TEST(test_delete_local, delete_records_e2e_fixture) {
     BOOST_CHECK_EQUAL("val1", consumed_records[0].second);
     BOOST_CHECK_EQUAL("key2", consumed_records[1].first);
     BOOST_CHECK_EQUAL("val2", consumed_records[1].second);
-    check_consume_out_of_range(model::offset(0));
+    check_consume_out_of_range(
+      consumer, topic_name, model::partition_id(0), model::offset(0));
+}
+
+FIXTURE_TEST(test_delete_from_archive, delete_records_e2e_fixture) {
+    auto partition = app.partition_manager.local().get(ntp);
+    auto* log = dynamic_cast<storage::disk_log_impl*>(
+      partition->log().get_impl());
+    auto archiver_ref = partition->archiver();
+    BOOST_REQUIRE(archiver_ref.has_value());
+    auto& archiver = archiver_ref.value().get();
+
+    kafka_produce_transport producer(make_kafka_client().get());
+    producer.start().get();
+
+    size_t total_records = 0;
+    const auto records_per_seg = 5;
+    const auto num_segs = 40;
+    while (partition->archival_meta_stm()->manifest().size() < num_segs) {
+        for (size_t i = 0; i < records_per_seg; i++) {
+            std::vector<kv_t> records;
+            records.emplace_back(
+              ssx::sformat("key{}", total_records),
+              ssx::sformat("val{}", total_records));
+            producer
+              .produce_to_partition(topic_name, model::partition_id(0), records)
+              .get();
+            total_records++;
+        }
+        log->flush().get();
+        log->force_roll(ss::default_priority_class()).get();
+        archiver.upload_next_candidates().get();
+    }
+    // 30 segments at 10 segments per spillover manifest gets 2 spillover
+    // manifests and 10 segments in the stm manifest.
+    archiver.apply_spillover().get();
+    auto& stm_manifest = archiver.manifest();
+    BOOST_REQUIRE_EQUAL(
+      stm_manifest.get_archive_start_offset(), model::offset(0));
+    BOOST_REQUIRE_GT(
+      stm_manifest.get_start_offset(), stm_manifest.get_archive_start_offset());
+    BOOST_REQUIRE_EQUAL(
+      stm_manifest.get_spillover_map().size(), num_segs / segs_per_spill - 1);
+    BOOST_REQUIRE_EQUAL(stm_manifest.size(), segs_per_spill);
+
+    kafka_delete_records_transport deleter(make_kafka_client().get());
+    deleter.start().get();
+    kafka_consume_transport consumer(make_kafka_client().get());
+    consumer.start().get();
+    for (size_t i = 1; i < total_records; i++) {
+        auto lwm = deleter
+                     .delete_records_from_partition(
+                       topic_name, model::partition_id(0), model::offset(i), 5s)
+                     .get();
+        BOOST_REQUIRE_EQUAL(model::offset(i), lwm);
+        check_consume_out_of_range(
+          consumer, topic_name, model::partition_id(0), model::offset(i - 1));
+        auto consumed_records = consumer
+                                  .consume_from_partition(
+                                    topic_name,
+                                    model::partition_id(0),
+                                    model::offset(i))
+                                  .get();
+        BOOST_REQUIRE(!consumed_records.empty());
+        auto key = consumed_records[0].first;
+        BOOST_REQUIRE_EQUAL(key, ssx::sformat("key{}", i));
+    }
+    // TODO(awong): make a more strict check for offset-based retention.
+    archiver.apply_archive_retention().get();
+    BOOST_REQUIRE_GT(stm_manifest.get_archive_start_offset(), model::offset(0));
 }
