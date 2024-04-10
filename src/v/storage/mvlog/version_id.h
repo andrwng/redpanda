d28344cacc61d40e6cc986063cf6c465d21d4a0e
Andrew Wong <awong@redpanda.com>
2024-04-10 00:04:45 -0700

    mvlog: add named type for version id
    This will uniquely identify new versions of the log, as indicated by
    suffix truncations.

---
+++ src/v/storage/mvlog/version_id.h
@@ -0,0 +1,16 @@
+// Copyright 2024 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+#pragma once
+
+#include "utils/named_type.h"
+
+namespace storage::experimental::mvlog {
+// Uniquely identifies the in-memory version of a versioned log.
+using version_id = named_type<int64_t, struct vid_tag>;
+} // namespace storage::experimental::mvlog
