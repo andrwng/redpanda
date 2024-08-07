6073c6e35ea40628771341d616b79070dd05fe41
Andrew Wong <awong@redpanda.com>
2024-08-06 21:25:21 -0700

    iceberg: add partition utils (partition schema)

---
+++ src/v/iceberg/partition_utils.cc
@@ -0,0 +1,38 @@
+// Copyright 2024 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+#include "iceberg/partition_utils.h"
+
+#include "iceberg/transform_utils.h"
+
+namespace iceberg {
+
+struct_type create_partition_type(
+  const partition_spec& partition_spec, const schema& schema) {
+    struct_type schema_struct;
+    const auto ids_to_types = schema.ids_to_types();
+    const auto& partition_fields = partition_spec.fields;
+    for (const auto& field : partition_fields) {
+        const auto& source_id = field.field_id;
+        const auto type_iter = ids_to_types.find(source_id);
+        if (type_iter == ids_to_types.end()) {
+            throw std::invalid_argument(fmt::format(
+              "Expected source field ID {} to be in schema", source_id()));
+        }
+        const auto& source_type = *type_iter->second;
+        auto result_field = nested_field::create(
+          field.field_id,
+          field.name,
+          field_required::yes,
+          get_result_type(source_type, field.transform));
+        schema_struct.fields.emplace_back(std::move(result_field));
+    }
+    return schema_struct;
+}
+
+} // namespace iceberg
