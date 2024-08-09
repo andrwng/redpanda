22e45cec8d42ed4de562506742157d732b27f233
Andrew Wong <awong@redpanda.com>
2024-08-08 23:03:03 -0700

    iceberg: hourly partition transform

--- src/v/iceberg/transform_utils.cc
+++ src/v/iceberg/transform_utils.cc
@@ -44,20 +44,47 @@ get_result_type(const field_type& source_type, const transform& transform) {
     return std::visit(transform_result_type_visitor{source_type}, transform);
 }
 
+namespace {
+
+static constexpr double hr_per_sec = 1.0 / 3600.0;
+int32_t micros_to_hr(int64_t micros) {
+    return static_cast<int32_t>(
+      static_cast<double>(micros) / 1000.0 / 1000.0 * hr_per_sec);
+}
+
+struct hourly_visitor {
+    int32_t operator()(const primitive_value& v) {
+        if (std::holds_alternative<timestamp_value>(v)) {
+            return micros_to_hr(std::get<timestamp_value>(v).val);
+        }
+        throw std::invalid_argument(fmt::format(
+          "hourly_visitor not implemented for primitive value {}", v));
+    }
+
+    template<typename T>
+    int32_t operator()(const T& t) {
+        throw std::invalid_argument(
+          fmt::format("hourly_visitor not implemented for value {}", t));
+    }
+};
+
+} // namespace
+
 struct transform_applying_visitor {
     explicit transform_applying_visitor(const value& source_val)
       : source_val_(source_val) {}
     const value& source_val_;
 
-    value_ptr operator()(const day_transform&) {
+    value_ptr operator()(const hour_transform&) {
         // TODO: implement me! And everything else.
-        int_value v{0};
+        int_value v{std::visit(hourly_visitor{}, source_val_)};
         return std::make_unique<value>(v);
     }
 
     template<typename T>
     value_ptr operator()(const T&) {
-        throw std::invalid_argument("Not supported");
+        throw std::invalid_argument(
+          "transform_applying_visitor not implemented for transform");
     }
 };
 
