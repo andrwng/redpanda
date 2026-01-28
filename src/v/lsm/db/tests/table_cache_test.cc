b12fbf17ae4a6e21acabe1ca61b28253e2159c40
Andrew Wong <awong@redpanda.com>
2026-01-28 09:01:10 -0800

    lsm/tests: unit test that crashes
    This test was generated by claude to try to reproduce the crash in
    db_domain_manager_test. Below is the decoded backtrace from the unit
    test:
    
      #0  seastar::guarded_backtrace()
      #1  seastar::backtrace<...>()
      #2  seastar::backtrace_buffer::append_backtrace()
      #3  seastar::print_with_backtrace()                    [reactor.cc]
      #4  seastar::print_with_backtrace()                    [reactor.cc]
      #5  seastar::sigsegv_action()                          [reactor.cc]
      #6  seastar::install_oneshot_signal_handler<SIGSEGV>() [reactor.cc]
      #7  seastar::install_oneshot_signal_handler<>::__invoke()
      #8  libc.so.6 (signal delivery)
    
      --- CRASH SITE ---
      #9  seastar::file::memory_dma_alignment() const        ← NULL file_impl!
      #10 lsm::io::disk_file_reader::read(offset, n)
    
      --- SST READER ---
      #11 lsm::sst::read_block(file_reader*, block_handle)   [reader.cc]
      #12 lsm::sst::reader::impl::block_reader() [coroutine] [reader.cc]
    
      --- TABLE CACHE ---
      #13 wrapped_iterator coroutine resume                  [table_cache.cc]
      #14 coroutine promise::run_and_dispose()
    
      --- SEASTAR REACTOR ---
      #15 seastar::reactor::task_queue::run_tasks()
      #16 seastar::reactor::task_queue_group::run_tasks()
      #17 seastar::reactor::task_queue_group::run_some_tasks()
      #18 seastar::reactor::do_run()
      #19 seastar::reactor::run()
      #20 seastar::app_template::run_deprecated()
      #21 seastar::app_template::run()
    
      --- TEST FRAMEWORK ---
      #22 seastar::testing::test_runner::start_thread()      [test_runner.cc]
      #23-#27 std::function invocation chain
    
      --- THREAD START ---
      #28 std::function<void()>::operator()()                [app-template.cc]
      #29 seastar::posix_thread::start_routine()
    
    Seems like we're closing a reader while it's still in use.
    
    Here's the backtrace from db_domain_manager_test
    --test_filter=*DroppedAppends/1 that it was trying to reproduce.
    
      --- SIGNAL HANDLER ---
      #0-8   Signal handling / backtrace capture
    
      --- CRASH SITE ---
      #9    seastar::file::close()                    ← Closing NULL file!
      #10   lsm::io::disk_file_reader::close()
    
      --- SST READER ---
      #11   lsm::sst::reader::impl::close()
      #12   lsm::sst::reader::close()
    
      --- TABLE CACHE CLEANUP ---
      #13   table_cache::impl::maybe_enqueue_cleanup()::{lambda}()  ← Cleanup lambda
      #14   seastar::noncopyable_function::call()
    
      --- WORK QUEUE ---
      #15   seastar::noncopyable_function::operator()()
      #16   ssx::work_queue::process() [coroutine]
    
      --- TEST ---
      #17   coroutine resume                          [db_domain_manager_test.cc]
      #18   promise::run_and_dispose()
    
      --- REACTOR ---
      #19-25 seastar::reactor task execution
    
      --- TEST FRAMEWORK ---
      #26-31 seastar::testing::test_runner
    
      --- STARTUP ---
      #32-36 app startup / libc

--- src/v/lsm/db/tests/table_cache_test.cc
+++ src/v/lsm/db/tests/table_cache_test.cc
@@ -11,19 +11,25 @@
 
 #include "base/seastarx.h"
 #include "lsm/core/internal/files.h"
+#include "lsm/core/internal/keys.h"
 #include "lsm/db/table_cache.h"
+#include "lsm/io/disk_persistence.h"
 #include "lsm/io/memory_persistence.h"
 #include "lsm/sst/builder.h"
 #include "test_utils/async.h"
+#include "utils/uuid.h"
 
 #include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
+
+#include <filesystem>
 
 namespace {
 
 using lsm::internal::file_handle;
 using lsm::internal::operator""_file_id;
 using lsm::internal::operator""_db_epoch;
+using lsm::internal::operator""_key;
 
 class TableCacheTest : public testing::Test {
 public:
@@ -33,6 +39,20 @@ public:
         auto file = _persistence->open_sequential_writer(filename).get();
         lsm::sst::builder builder(std::move(file), {});
         // Just make empty SST files - the cache doesn't care about the contents
+        builder.finish().get();
+        builder.close().get();
+        return std::make_pair(filename, builder.file_size());
+    }
+
+    // Create an SST file with actual data that requires I/O to read
+    std::pair<lsm::internal::file_handle, size_t> make_sst_with_data() {
+        auto filename = file_handle{.id = ++_latest_id, .epoch = 0_db_epoch};
+        auto file = _persistence->open_sequential_writer(filename).get();
+        lsm::sst::builder builder(std::move(file), {});
+        // Add some key-value pairs
+        builder.add("key1"_key, iobuf::from("value1")).get();
+        builder.add("key2"_key, iobuf::from("value2")).get();
+        builder.add("key3"_key, iobuf::from("value3")).get();
         builder.finish().get();
         builder.close().get();
         return std::make_pair(filename, builder.file_size());
@@ -52,6 +72,48 @@ private:
     lsm::internal::file_id _latest_id;
     std::unique_ptr<lsm::io::data_persistence> _persistence
       = lsm::io::make_memory_data_persistence();
+};
+
+// Test fixture that uses disk persistence to reproduce real I/O behavior
+class TableCacheDiskTest : public testing::Test {
+public:
+    void SetUp() override {
+        std::filesystem::path tmpdir = std::getenv("TEST_TMPDIR");
+        auto subdir = ss::sstring(uuid_t::create());
+        _persistence = lsm::io::open_disk_data_persistence(
+                         tmpdir / std::string_view(subdir))
+                         .get();
+    }
+
+    std::pair<lsm::internal::file_handle, size_t> make_sst_with_data() {
+        auto filename = file_handle{.id = ++_latest_id, .epoch = 0_db_epoch};
+        auto file = _persistence->open_sequential_writer(filename).get();
+        lsm::sst::builder builder(std::move(file), {});
+        // Add some key-value pairs
+        builder.add("key1"_key, iobuf::from("value1")).get();
+        builder.add("key2"_key, iobuf::from("value2")).get();
+        builder.add("key3"_key, iobuf::from("value3")).get();
+        builder.finish().get();
+        builder.close().get();
+        return std::make_pair(filename, builder.file_size());
+    }
+
+    lsm::db::table_cache make_table_cache(size_t max_entries = 10) {
+        return {
+          _persistence.get(),
+          max_entries,
+          ss::make_lw_shared<lsm::sst::block_cache>(1_MiB)};
+    }
+
+    void TearDown() override {
+        if (_persistence) {
+            _persistence->close().get();
+        }
+    }
+
+private:
+    lsm::internal::file_id _latest_id;
+    std::unique_ptr<lsm::io::data_persistence> _persistence;
 };
 
 } // namespace
@@ -109,5 +171,33 @@ TEST_F(TableCacheTest, MaxEntriesWithOpenIterators) {
     // But once everything is cleaned up, we only have 7 things enqueued (for
     // why 7 see the comment inMaxEntries).
     EXPECT_EQ(cache.statistics().open_file_handles, 7) << cache.statistics();
+    cache.close().get();
+}
+
+// This test reproduces a crash where evict() closes a reader while an iterator
+// is still using it. The crash occurs because the iterator tries to read from
+// a closed file.
+//
+// Uses disk persistence because memory persistence doesn't invalidate state
+// on close the same way real disk files do.
+TEST_F(TableCacheDiskTest, EvictWithActiveIteratorCrashes) {
+    auto cache = make_table_cache();
+    auto [handle, size] = make_sst_with_data();
+
+    // Create an iterator - this holds a reference to the reader
+    auto iter = cache.create_iterator(handle, size).get();
+
+    // Evict the file while the iterator is still alive.
+    // This will close the underlying reader.
+    cache.evict(handle).get();
+
+    // Now try to use the iterator - this should crash because the reader
+    // has been closed but the iterator still tries to use it.
+    // The crash manifests as a segfault when accessing the closed file's
+    // memory_dma_alignment (offset 8 in the file object).
+    iter->seek_to_first().get();
+
+    // If we get here without crashing, the bug is fixed
+    EXPECT_TRUE(iter->valid());
     cache.close().get();
 }
