fb7cee00970ec76f6c6222e0675b18479f508a6a
Andrew Wong <awong@redpanda.com>
2026-04-14 10:12:30 -0700

    iceberg: add position delete file writer
    Writes Iceberg V2 position delete files in parquet format with
    file_path and pos columns. Uses the Iceberg spec-reserved field IDs
    from MetadataColumns (Integer.MAX_VALUE - 101/102).

---
+++ src/v/iceberg/position_delete_file.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2026 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+#pragma once
+
+#include "bytes/iobuf.h"
+#include "container/chunked_vector.h"
+#include "iceberg/manifest_entry.h"
+#include "iceberg/partition_key.h"
+#include "iceberg/uri.h"
+
+#include <seastar/core/future.hh>
+
+namespace iceberg {
+
+struct position_delete_entry {
+    uri file_path;
+    int64_t pos;
+    partition_key partition;
+};
+
+struct delete_file_result {
+    iobuf file_data;
+    data_file manifest_entry;
+};
+
+/// \brief Write a position delete file in parquet format from sorted entries.
+///
+/// Entries MUST be sorted by file_path then pos (Iceberg spec requirement).
+/// The output parquet schema is:
+///   file_path (required BYTE_ARRAY, string logical type)
+///   pos       (required INT64)
+ss::future<delete_file_result> write_position_delete_file(
+  chunked_vector<position_delete_entry> deletes, bool compress = false);
+
+} // namespace iceberg
